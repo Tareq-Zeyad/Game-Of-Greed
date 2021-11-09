f34b4e6f68f8933e803b7864a789f819f86a8b05
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2021-11-09 15:40:19 +0200

    tests not completed

--- tests/test_game_flo.py
+++ tests/test_game_flo.py
@@ -7,29 +7,31 @@ from game_of_greed.game import Game
 pytestmark = [pytest.mark.flow]
 
 
-def test_quitter():
-    game = Game()
-    diffs = Flo(game.play, path="tests/flow/quitter.sim.txt")
-    assert not diffs, diffs
+# def test_quitter():
+#     game = Game()
+#     diffs = Flo(game.play, path="tests/flow/quitter.sim.txt")
+#     assert not diffs, diffs
 
 
-def test_one_and_done():
-    game = Game()
-    diffs = Flo(game.play, path="tests/flow/one_and_done.sim.txt")
-    assert not diffs, diffs
+# def test_one_and_done():
+#     game = Game()
+#     diffs = Flo(game.play, path="tests/flow/one_and_done.sim.txt")
+#     assert not diffs, diffs
 
 
-def test_single_bank():
-    game = Game()
-    diffs = Flo(
-        game.play, path="tests/flow/bank_one_roll_then_quit.sim.txt"
-    )
-    assert not diffs, diffs
+# def test_single_bank():
+#     game = Game()
+#     diffs = Flo(
+#         game.play, path="tests/flow/bank_one_roll_then_quit.sim.txt"
+#     )
+#     assert not diffs, diffs
 
 
 def test_bank_first_for_two_rounds():
-    game = Game()
-    diffs = Flo(
-        game.play, path="tests/flow/bank_first_for_two_rounds.sim.txt"
-    )
-    assert not diffs, diffs
+
+    Flo.test('tests/flow/bank_first_for_two_rounds.sim.txt')
+    # game = Game()
+    # diffs = Flo(
+    #     game.play, path="tests/flow/bank_first_for_two_rounds.sim.txt"
+    # )
+    # assert not diffs, diffs
